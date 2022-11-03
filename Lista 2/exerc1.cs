1eb9cd89b76c4f2034a277e8df3ebad1feb3ea5b
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2022-11-03 09:36:24 -0300

    Create exerc1.cs

---
+++ Lista 2/exerc1.cs
@@ -0,0 +1,8 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Digite seu primeiro nome:");
+    string s = Console.ReadLine();
+    Console.WriteLine($"Bem vindo ao C#, {s}");
+  }
+}
